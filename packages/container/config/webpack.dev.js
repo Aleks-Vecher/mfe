48eda13064d97f9d9abf61f726c5fc0567456a9a
Aleks-Vecher <aleks.vecher@gmail.com>
2026-05-31 09:41:32 +0300

    feat: added dashboard

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -19,6 +19,7 @@ const devConfig = {
         // 'marketing@http://localhost:8081/remoteEntry.js' : marketing is the name from another frontend of dev webpack moduleFederation plugin
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
         auth: "auth@http://localhost:8082/remoteEntry.js",
+        dashboard: "dashboard@http://localhost:8083/remoteEntry.js",
       },
       shared: packageJson.dependencies,
     }),
